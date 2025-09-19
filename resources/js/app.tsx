48a1b93f5346848bc078d7ec9962f6af2fe5fdf9
= <=>
2025-09-20 01:54:59 +0700

    success

---
+++ resources/js/app.tsx
@@ -0,0 +1,18 @@
+import {Icon} from './components/icon'
+
+const App = () => {
+
+    return (
+        <>
+        
+        <div className='container'>
+            <Icon iconNode={}></Icon>
+        </div>
+
+        
+        </>
+    )
+
+}
+
+export default App
